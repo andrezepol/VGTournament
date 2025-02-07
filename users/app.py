0d547d9e632e007c6ca12cc2ba8becce915a5883
andrezepol <andresjimenezl50@gmail.com>
2025-02-06 21:44:51 -0500

    Change directory name

--- users/app.py
+++ users/app.py
@@ -1,12 +1,42 @@
 import json
 
+# import requests
+
+
 def lambda_handler(event, context):
-    body = json.loads(event.get("body", "{}"))
+    """Sample pure Lambda function
 
-    # Aquí iría la lógica para crear el usuario
-    response = {
-        "statusCode": 201,
-        "body": json.dumps({"message": "Usuario creado con éxito", "user": body})
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
     }
-
-    return response
